10477072e6898c55878078ab052718466efd095d
unknown <diwserala@gmail.com>
2022-01-19 15:32:08 +0545

    Added securities

--- src/main/java/com/example/diwakar/jwtservice/JwtserviceApplication.java
+++ src/main/java/com/example/diwakar/jwtservice/JwtserviceApplication.java
@@ -6,6 +6,9 @@ import com.example.diwakar.jwtservice.service.AppUserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.ArrayList;
 
@@ -16,27 +19,34 @@ public class JwtserviceApplication {
 		SpringApplication.run(JwtserviceApplication.class, args);
 	}
 
-	CommandLineRunner run(AppUserService appUserService) {
-		return args -> {
-			appUserService.saveRole(new Role(null, "ROLE_USER"));
-			appUserService.saveRole(new Role(null, "ROLE_MANAGER"));
-			appUserService.saveRole(new Role(null, "ROLE_ADMIN"));
-			appUserService.saveRole(new Role(null, "ROLE_SUPERADMIN"));
-
-			appUserService.saveUser(new AppUser(null, "John Travolta", "john", "1234", new ArrayList<>()));
-			appUserService.saveUser(new AppUser(null, "Will Smith", "will", "1234", new ArrayList<>()));
-			appUserService.saveUser(new AppUser(null, "Jim Carry", "jim", "1234", new ArrayList<>()));
-			appUserService.saveUser(new AppUser(null, "Arnold Schwarzenegger", "arnold", "1234", new ArrayList<>()));
-
-			appUserService.addRoleToUser("john", "ROLE_USER");
-			appUserService.addRoleToUser("john", "ROLE_MANAGER");
-			appUserService.addRoleToUser("will", "ROLE_MANAGER");
-			appUserService.addRoleToUser("jim", "ROLE_ADMIN");
-			appUserService.addRoleToUser("arnold", "ROLE_SUPERADMIN");
-			appUserService.addRoleToUser("arnold", "ROLE_ADMIN");
-			appUserService.addRoleToUser("arnold", "ROLE_USER");
-
-		};
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
+
+
+//	@Bean
+//	CommandLineRunner run(AppUserService appUserService) {
+//		return args -> {
+//			appUserService.saveRole(new Role(null, "ROLE_USER"));
+//			appUserService.saveRole(new Role(null, "ROLE_MANAGER"));
+//			appUserService.saveRole(new Role(null, "ROLE_ADMIN"));
+//			appUserService.saveRole(new Role(null, "ROLE_SUPERADMIN"));
+//
+//			appUserService.saveUser(new AppUser(null, "John Travolta", "john", "1234", new ArrayList<>()));
+//			appUserService.saveUser(new AppUser(null, "Will Smith", "will", "1234", new ArrayList<>()));
+//			appUserService.saveUser(new AppUser(null, "Jim Carry", "jim", "1234", new ArrayList<>()));
+//			appUserService.saveUser(new AppUser(null, "Arnold Schwarzenegger", "arnold", "1234", new ArrayList<>()));
+//
+//			appUserService.addRoleToUser("john", "ROLE_USER");
+//			appUserService.addRoleToUser("john", "ROLE_MANAGER");
+//			appUserService.addRoleToUser("will", "ROLE_MANAGER");
+//			appUserService.addRoleToUser("jim", "ROLE_ADMIN");
+//			appUserService.addRoleToUser("arnold", "ROLE_SUPERADMIN");
+//			appUserService.addRoleToUser("arnold", "ROLE_ADMIN");
+//			appUserService.addRoleToUser("arnold", "ROLE_USER");
+//
+//		};
+//	}
 
 }
--- src/main/java/com/example/diwakar/jwtservice/service/AppUserServiceImpl.java
+++ src/main/java/com/example/diwakar/jwtservice/service/AppUserServiceImpl.java
@@ -6,19 +6,42 @@ import com.example.diwakar.jwtservice.repo.AppUserRepo;
 import com.example.diwakar.jwtservice.repo.RoleRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 @Slf4j
-public class AppUserServiceImpl implements AppUserService {
+public class AppUserServiceImpl implements AppUserService, UserDetailsService {
 
     private final AppUserRepo appUserRepo;
     private final RoleRepo roleRepo;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AppUser appUser = appUserRepo.findByUsername(username);
+        if(appUser == null) {
+            log.error("User not found the database");
+            throw new UsernameNotFoundException("User not found the database");
+        } else {
+            log.info("User found in the database: {}", username);
+        }
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        appUser.getRoles().forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        });
+        return new org.springframework.security.core.userdetails.User(appUser.getUsername(), appUser.getPassword(), authorities);
+    }
 
     @Override
     public AppUser saveUser(AppUser user) {
@@ -53,4 +76,5 @@ public class AppUserServiceImpl implements AppUserService {
         log.info("Fetching all users");
         return appUserRepo.findAll();
     }
+
 }
